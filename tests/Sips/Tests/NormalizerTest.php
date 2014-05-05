00bc7d7ad6cc2b886b8a694bbd6ff2c54016dc94
jelte <jelte@marlon.be>
2014-05-05 10:13:23 +0200

    Normalize "<" & ">" to " "

--- tests/Sips/Tests/NormalizerTest.php
+++ tests/Sips/Tests/NormalizerTest.php
@@ -14,4 +14,13 @@ class NormalizerTest extends \TestCase {
         $string = "Màthìéû Dùffèlêr";
         $this->assertEquals("Mathieu Duffeler", Normalizer::normalize($string));
     }
-} 
+
+    /**
+     * @test
+     */
+    public function CanNormalizeHTMLTags()
+    {
+        $string = "Màthìéû Dùffèlêr<a href=\"test\">test</a>";
+        $this->assertEquals("Mathieu Duffeler a href=\"test\" test /a ", Normalizer::normalize($string));
+    }
+}
